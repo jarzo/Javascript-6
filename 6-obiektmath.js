6238b4b22fd41cc14d0bd1dedc598faf49ef0749
Michał Jarząbek <nebraska994@gmail.com>
2017-06-10 11:54:18 +0200

    console

---
+++ 6-obiektmath.js
@@ -0,0 +1,15 @@
+'use strict'
+
+
+/*
+
+Obiekt Math
+
+*/
+//Pole Kola = Pi * r2
+var r = 10;
+console.log( Math.PI * Math.pow(r, 2) );
+
+console.log( Math.cos(0) );
+
+console.log("LN10 =" + Math.LN10);
